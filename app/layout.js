4e23774ee2a90b1ce3c6d1e04b456d7fe6e4a11b
Sushant Kumar <124432314+sjha34471234@users.noreply.github.com>
2026-05-18 15:53:42 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -45,7 +45,11 @@ export const metadata = {
     follow: true,
   },
 };
-
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
